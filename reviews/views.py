69457d2e8109018dbd99862c3f4a360cb1bdd5cb
rafsun-riad <mruhaquer@gmail.com>
2024-03-12 19:24:03 +0600

    learning about django form

--- reviews/views.py
+++ reviews/views.py
@@ -7,8 +7,11 @@ from django.shortcuts import render
 def review(request):
     if request.method == 'POST':
         entered_username = request.POST['username']
+        if entered_username == "":
+            return render(request, "reviews/review.html", {"has_error": True})
         return HttpResponseRedirect("/thank-you")
-    return render(request, "reviews/review.html")
+
+    return render(request, "reviews/review.html", {"has_error": False})
 
 
 def thank_you(request):
