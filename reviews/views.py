f385a1262d1bc2a8e31e90360b133040182e27ba
rafsun-riad <mruhaquer@gmail.com>
2024-03-20 02:26:39 +0600

    learned about form model & class view

--- reviews/views.py
+++ reviews/views.py
@@ -1,30 +1,24 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import ReviewForm
-from .models import Review
+from django.views import View
+
 
 # Create your views here.
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
 
+        return render(request, "reviews/review.html", {
+            "form": form,
+        })
 
-def review(request):
-    if request.method == 'POST':
+    def post(self, request):
         form = ReviewForm(request.POST)
 
         if form.is_valid():
-            review_db = Review(
-                user_name=form.cleaned_data['user_name'],
-                review_text=form.cleaned_data['review_text'],
-                rating=form.cleaned_data['rating']
-            )
-            review_db.save()
+            form.save()
             return HttpResponseRedirect("/thank-you")
-
-    else:
-        form = ReviewForm()
-
-    return render(request, "reviews/review.html", {
-        "form": form,
-    })
 
 
 def thank_you(request):
