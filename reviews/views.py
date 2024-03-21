93cb5c5420a82492ddbbcd38f1f0b706b8a99e3f
rafsun-riad <mruhaquer@gmail.com>
2024-03-22 01:23:34 +0600

    learned more about template view

--- reviews/views.py
+++ reviews/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from .forms import ReviewForm
 from django.views import View
 from django.views.generic.base import TemplateView
+from .models import Review
 
 
 # Create your views here.
@@ -28,4 +29,25 @@ class ThankYouView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["message"] = "This works"
+        return context
+
+
+class ReviewsListView(TemplateView):
+    template_name = "reviews/review_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        reviews = Review.objects.all()
+        context["reviews"] = reviews
+        return context
+
+
+class SingleReviewView(TemplateView):
+    template_name = "reviews/single_review.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        review_id = kwargs["id"]
+        selected_review = Review.objects.get(pk=review_id)
+        context["reviews"] = selected_review
         return context
